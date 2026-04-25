d6dbc651ecf83fa50f0419d81bac3221486d009c
Sar-Hal <sar656hal@gmail.com>
2026-04-25 13:50:21 +0530

    Minor fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { motion } from 'motion/react';
 const navItems = [
   { name: 'Home', href: '#home' },
   { name: 'About', href: '#about' },
-  { name: 'Skills', href: '#skills' },
+  { name: 'Experience', href: '#experience' },
   { name: 'Projects', href: '#projects' },
   { name: 'Contact', href: '#contact' },
 ];
